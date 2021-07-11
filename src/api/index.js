51b0a4686cf3f83a20b27f168b666cf0bd171cb8
Seymen Kılıç <78303613+seymenklc@users.noreply.github.com>
2021-07-11 21:05:49 +0300

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,7 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 export const fetchData = async (queryString, unit) => {
     try {
         const { data } = await axios.get(
-            `http://api.openweathermap.org/data/2.5/weather?q=${queryString}&units=${unit}&appid=${API_KEY}`
+            `https://api.openweathermap.org/data/2.5/weather?q=${queryString}&units=${unit}&appid=${API_KEY}`
         );
         return data;
     } catch (error) {
